01f4275a91386e735bb96351970b2ed6e07bc801
Zokir <zokhir.s@mail.ru>
2018-08-16 01:42:21 -0400

    second

--- src/test/java/com/qa/runners/CukesRunner.java
+++ src/test/java/com/qa/runners/CukesRunner.java
@@ -7,12 +7,19 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+		plugin = {
+				"pretty",
+				"html:target/default-cucumber-reports", 
+				"json:target/cucumber.json"
+		
+		},
+		
 		tags="@temp",
 		features= {"src/test/resources/com/fetures"},
-		glue= {
-			"src/test/java/com/qa/step_definitions/HomePageStepDef.java"	
-		}
-		,dryRun=false
+		glue= 
+			"src\\test\\java\\com\\qa\\step_definitions"	
+		
+		//,dryRun=false
 		
 		)
 public class CukesRunner {
